2010371ab553e50b8fda624516e6e63c531a1ae1
meshhi <yumythug@yandex.ru>
2023-10-23 23:12:45 +0300

    audio-record

--- src/js/components/message/Message.js
+++ src/js/components/message/Message.js
@@ -4,11 +4,21 @@ class Message {
     constructor() {
       this.message = document.createElement('div');
       this.message.classList.add('msg');
+      this.text = document.createElement('div');
+      this.text.classList.add('msg-text');
+      this.message.appendChild(this.text);
+      this.geolocationContainer = document.createElement('div');
+      this.geolocationContainer.classList.add('geolocation-container');
+      this.message.appendChild(this.geolocationContainer);
     }
   
     bindToDOM = (container) => {
       this.container = container;
       this.container.appendChild(this.message);
+    }
+
+    addGeolocation = (latitude, longitude) => {
+      this.geolocationContainer.textContent = `${latitude} ${longitude}`
     }
   }
   
--- src/js/components/message/TextMessage.js
+++ src/js/components/message/TextMessage.js
@@ -3,7 +3,7 @@ import Message from "./Message";
 class TextMessage extends Message {
     constructor(text) {
       super();
-      this.message.textContent = text;
+      this.text.textContent = text;
     }
   }
   
